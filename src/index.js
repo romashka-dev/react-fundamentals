430217449455856c4c56a715533b4615763a04be
romashka-dev <romikkbox@gmail.com>
2024-12-07 15:06:13 +0200

    [feat]: basic setup of props

--- src/index.js
+++ src/index.js
@@ -3,41 +3,30 @@ import ReactDOM from 'react-dom/client'
 
 import './index.css'
 
+const image = 'https://m.media-amazon.com/images/I/71K+9YJ7VbL._SY522_.jpg'
+const title =
+  'Dog Man: Big Jim Begins: A Graphic Novel (Dog Man #13): From the Creator of Captain Underpants'
+const author = 'Dav Pilkey'
+
 const BookList = () => {
   return (
     <section className="booklist">
-      <Book />
+      <Book image={image} title={title} author={author} />
+      <Book image={image} title={title} author={author} />
+      <Book image={image} title={title} author={author} />
     </section>
   )
 }
 
-const Book = () => {
+const Book = (props) => {
   return (
     <article className="book">
-      <Image />
-      <Title />
-      <Author />
+      <img src={props.image} alt={props.title} width={192} height={285} />
+      <h2>{props.title}</h2>
+      <h2>{props.author}</h2>
     </article>
   )
 }
-
-const Image = () => (
-  <img
-    src="https://m.media-amazon.com/images/I/71K+9YJ7VbL._SY522_.jpg"
-    alt="Dog Man: Big Jim Begins: A Graphic Novel (Dog Man #13): From the Creator of Captain Underpants"
-    width={192}
-    height={285}
-  />
-)
-
-const Title = () => (
-  <h2>
-    Dog Man: Big Jim Begins: A Graphic Novel (Dog Man #13): From the Creator of
-    Captain Underpants
-  </h2>
-)
-
-const Author = () => <h2>Dav Pilkey</h2>
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 
