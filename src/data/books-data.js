c913ad83fdf89089adb524c5577e9851dc2396bf
romashka-dev <romikkbox@gmail.com>
2024-12-11 15:59:06 +0200

    [chore]: split logic on files | ES Modules

---
+++ src/data/books-data.js
@@ -0,0 +1,25 @@
+export const BOOKS = [
+  {
+    id: 1,
+    image: 'https://m.media-amazon.com/images/I/71AxeLOhKTL._SY522_.jpg',
+    title: `Dad Jokes: Over 600 of the Best (Worst) Jokes Around and Perfect Christmas Gag Gift for All Ages! (World's Best Dad Jokes Collection)`,
+    author: 'Jimmy Niro',
+    description:
+      'A USA Today bestseller! Dad Jokes: the good, the bad, and the terrible―the perfect funny Christmas gift for dads and pun-lovers alike!',
+  },
+  {
+    id: 2,
+    image: 'https://m.media-amazon.com/images/I/81OkWjcf4WL._SY522_.jpg',
+    title: 'The Nightingale: A Novel',
+    author: 'Kristin Hannah',
+    description:
+      'A #1 New York Times bestseller, Wall Street Journal Best Book of the Year, and soon to be a major motion picture, this unforgettable novel of love and strength in the face of war has enthralled a generation.',
+  },
+  {
+    id: 3,
+    image: 'https://m.media-amazon.com/images/I/81kTEpq8k+L._SY522_.jpg',
+    title: `Things I Can't Say Out Loud In Meetings: Funny Notebook for Work, Gag Gift, Boss, Office, Secret Santa Gift for Coworker (Lined Journal with Quotes)`,
+    author: 'Taylor A. Wilde',
+    description: `A Fun Office Notebook: Perfect for Gifting to Coworkers, Colleagues, Employees, or your Boss! This journal features a humorous cover, lined journal pages with snarky/sarcastic quotes on alternate pages, and also a “This Book Belongs To” page which you can personalize if you're giving as a gift.`,
+  },
+]
--- src/index.js
+++ src/index.js
@@ -1,34 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-
+import { BOOKS } from './data/books-data'
 import './index.css'
-
-const BOOKS = [
-  {
-    id: 1,
-    image: 'https://m.media-amazon.com/images/I/71K+9YJ7VbL._SY522_.jpg',
-    title:
-      'Dog Man: Big Jim Begins: A Graphic Novel (Dog Man #13): From the Creator of Captain Underpants',
-    author: 'Dav Pilkey',
-    description:
-      'In DOG MAN: BIG JIM BEGINS, discover the origin of our beloved characters from the Dog Man series as they join forces to stop the Space Cuties from destroying the city. Will the past predict the future for Dog Man and his friends? Will goodness and bravery prevail? Can anything happen if you truly believe?',
-  },
-  {
-    id: 2,
-    image: 'https://m.media-amazon.com/images/I/41e+dwz5W5L._SY522_.jpg',
-    title: 'Melania Hardcover – October 8, 2024',
-    author: 'Melania Trump',
-    description:
-      'Melania is a compelling and inspirational memoir that offers a glimpse into the life of a remarkable woman who has navigated challenges with grace and determination.',
-  },
-  {
-    id: 3,
-    image: 'https://m.media-amazon.com/images/I/81paxLbonYL._SY522_.jpg',
-    title: `From Crook to Cook: Platinum Recipes from Tha Boss Dogg's Kitchen (Snoop Dogg Cookbook, Celebrity Cookbook with Soul Food Recipes) (Snoop Dog x Chronicle Books)`,
-    author: 'Snoop Dogg',
-    description: `Looking for cookbooks with a little more personality? Welcome to tha Boss Dogg's Kitchen.`,
-  },
-]
 
 const BookList = () => {
   return (
@@ -51,7 +24,7 @@ const Book = (props) => {
     <article key={id} className="book">
       <img src={image} alt={title} width={192} height={285} />
       <h2>{title}</h2>
-      <h2>{author}</h2>
+      <h2 className="author">{author}</h2>
       {children}
     </article>
   )
